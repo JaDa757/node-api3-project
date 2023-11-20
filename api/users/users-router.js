7fca2ffe5c431cd6d6d6782a7ebe3dd798829ba3
Joshua Ackerman <joshackerman757@gmail.com>
2023-11-20 14:06:17 -0500

    get posts

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -12,10 +12,10 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   User.get()
-  .then(users => {
-    res.json(users)
-  })
-  .catch(next)
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
@@ -24,35 +24,39 @@ router.get('/:id', validateUserId, (req, res) => {
 
 router.post('/', validateUser, (req, res, next) => {
   User.insert({ name: req.name })
-  .then(newUser => {    
-    res.status(201).json(newUser)
-  })
-  .catch(next)
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(next)
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res, next) => {
-  User.update(req.params.id, {name: req.name })
-  .then(() => {
-    return User.getById(req.params.id)
-  })
-  .then(user => {
-    res.json(user)
-  })
-  .catch(next)
+  User.update(req.params.id, { name: req.name })
+    .then(() => {
+      return User.getById(req.params.id)
+    })
+    .then(user => {
+      res.json(user)
+    })
+    .catch(next)
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
-  console.log(req.user)
-
+router.delete('/:id', validateUserId, async (req, res, next) => {
+  try {
+    await User.remove(req.params.id)
+    res.json(req.user)
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
-  console.log(req.user)
-
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
+ try {
+const result = await User.getUserPosts(req.params.id)
+res.json(result)
+ } catch (err) {
+  next(err)
+ }
 });
 
 router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
