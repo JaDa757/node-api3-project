4520ec32769cb50b707cdd14bb175dc40a95ed59
Joshua Ackerman <joshackerman757@gmail.com>
2023-11-20 12:54:48 -0500

    server start

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,8 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get(
+    'Origin'
+  )}`
+  );
 }
 
 function validateUserId(req, res, next) {
@@ -14,4 +17,9 @@ function validatePost(req, res, next) {
   // DO YOUR MAGIC
 }
 
-// do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
--- api/server.js
+++ api/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const middlewares = require('./middleware/middleware.js')
+
 
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
+server.use(express.json())
+
 
 // global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+
+// router.use((error, req, res, next) => {
+//   res.status(error.status || 500).json({
+//     message: error.message,
+//     customMessage: "Something went wrong here"
+//   })
+// })
+
 
 module.exports = server;
